1f546e9916cbfe0d4fab14cb1564bfe2e90da037
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2014-09-24 23:50:36 -0400

    Rename

--- tree_cores/quickcorecheck.R
+++ tree_cores/quickcorecheck.R
@@ -4,23 +4,30 @@
 library(ggplot2)
 theme_set(theme_bw())
 
+# Read core data
 coredata <- read.csv("tree_cores.csv")
 coredata$type <- "cores"
 coredata <- coredata[c("Transect", "Position", "Species", "DBH_cm", "type")]
 
+# Read inventory data and filter for what we want
 inventorydata <- read.csv("../tree_survey/tree_survey.csv")
 inventorydata$type <- "inventory"
-inventorydata <- inventorydata[c("Transect", "Position", "Species", "DBH_cm", "type")]
-
+inventorydata <- inventorydata[inventorydata$Status=="Alive", c("Transect", "Position", "Species", "DBH_cm", "type")]
 
 d <- rbind(coredata, inventorydata)
 
 # Graph
-p <- ggplot(d, aes(factor(Position), DBH_cm, color=type)) 
-p <- p + geom_violin() + facet_grid(Transect~.)
-p <- p + xlab("Transect position (m)")
-print(p)
-#ggsave("corecheck.pdf")
+p1 <- ggplot(d, aes(factor(Position), DBH_cm, color=type)) 
+p1 <- p1 + geom_boxplot() + facet_grid(Transect~.)
+p1 <- p1 + xlab("Transect position (m)")
+print(p1)
+#ggsave("corecheck1.pdf")
+
+p2 <- ggplot(d, aes(DBH_cm)) + geom_density(aes(fill=type), alpha=.5)
+p2 <- p2 + facet_grid(Transect~.)
+print(p2)
+#ggsave("corecheck2.pdf")
+
 
 # Report
 print("Where does inventory DBH exceed core DBH?")
@@ -29,7 +36,7 @@ dsum <- ddply(d, .(Transect,Position,type), summarise, max_DBH=max(DBH_cm))
 library(reshape2)
 dsum <- na.omit(dcast(dsum,Transect+Position~type))
 dsum$Exceeds <- ""
-dsum[dsum$inventory>dsum$cores, "Exceeds"] <- "***"
+dsum[dsum$inventory-dsum$cores > 1, "Exceeds"] <- "***"
 print(dsum)
 
 print("All done.")
