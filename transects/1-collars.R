498acf52a5ef1ed9d4148a5edf36acf091bae112
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-02-26 20:45:04 -0500

    Transect data and code for map

--- transects/1-collars.R
+++ transects/1-collars.R
@@ -33,10 +33,21 @@ tdata$Transect <- paste0("T", tdata$Transect)
 printlog("Creating plotting data...")
 collarmap <- expand.grid(Transect=tdata$Transect, Collar=unique(cdata$Collar))
 collarmap <- merge(merge(collarmap, cdata), tdata)
+collarmap$MainCollar <- collarmap$Position_m %in% c(0, 4, 12, 20, 24, 32, 40, 44, 52, 60, 64, 72)
+
 savedata(collarmap, scriptfolder=FALSE)
 
-p <- ggplot(collarmap, aes(Position_SN, Position_m)) + geom_point()
-p <- p + xlab("Meters (south to north)") + ylab("Meters (east to west)")
+
+
+p <- ggplot(collarmap, aes(Position_m, Position_SN, size=MainCollar)) + geom_point()
+p <- p + ylab("Meters (south to north)") + xlab("Meters (west to east)")
+p <- p + scale_size_discrete(guide=FALSE)
+t1 <- 20
+t2 <- 30
+p <- p + annotate(geom="rect", ymin=-10, ymax=85, xmin=-10, xmax=t1, alpha=0.1)
+p <- p + annotate(geom="rect", ymin=-10, ymax=85, xmin=t1, xmax=t2, alpha=0.2)
+p <- p + annotate(geom="rect", ymin=-10, ymax=85, xmin=t2, xmax=85, alpha=0.3)
+p <- p + coord_equal(xlim=c(-5, 75), ylim=c(-5, 75))
 print(p)
 saveplot("collarmap", scriptfolder=FALSE)
 
